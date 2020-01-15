a479d73b20592d5ff0a8af5cc54d2e269a080eee
arnoldtjiawi <arnoldtjiawi@gmail.com>
2020-01-16 05:17:10 +0700

    Learning Code

--- DelayCode/ViewController.swift
+++ DelayCode/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        //1. delay with perform
+        perform(#selector(self.whatsGoodie), with: nil, afterDelay: 2)
+     
+        //2. dispatch queue
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+4){
+            self.animateBackgroundColor(to: .yellow)
+        }
+        
+        //3. timer schedule timer
+        Timer.scheduledTimer(withTimeInterval: 6, repeats: false) { (_) in
+            self.animateBackgroundColor(to: .cyan)
+        }
+        
     }
-
+    @objc func whatsGoodie(){
+        animateBackgroundColor(to: .red)
+    }
+    fileprivate func animateBackgroundColor(to color: UIColor){
+        UIView.animate(withDuration: 0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.view.backgroundColor = color
+        })
+    }
 
 }
 
